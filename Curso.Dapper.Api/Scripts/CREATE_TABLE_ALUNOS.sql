0880871c35000f25105e033a33d06f1d55a61c55
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2023-07-10 22:26:49 -0300

    Adicionar arquivos de projeto.

---
+++ Curso.Dapper.Api/Scripts/CREATE_TABLE_ALUNOS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Alunos(
+    Id INTEGER PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    DataNascimento DATETIME NOT NULL,
+    Ativo BIT NOT NULL,
+    DataCriacao DATETIME NOT NULL,
+    Curso VARCHAR(50),
+    Turma VARCHAR(50),
+    Turno VARCHAR(50)
+)
