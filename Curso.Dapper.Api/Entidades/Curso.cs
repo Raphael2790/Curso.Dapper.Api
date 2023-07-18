9e0a59c46806d842b2a86d6a5affa0fd3cd945a7
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2023-07-17 21:53:55 -0300

    Usando dynamics simples e com tabelas relacionadas, selects multiplos na mesma consulta, select com relacionamentos (1,1) (1, n)  (n, n)

---
+++ Curso.Dapper.Api/Entidades/Curso.cs
@@ -0,0 +1,13 @@
+﻿namespace Curso.Dapper.Api.Entidades;
+
+public class Curso
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string Coordenador { get; set; }
+    public string Professor { get; set; }
+    public DateTime DataCriacao { get; set; }
+
+    public IEnumerable<Turma> Turmas { get; set; }
+}
