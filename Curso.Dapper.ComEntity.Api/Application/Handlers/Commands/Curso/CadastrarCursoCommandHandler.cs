999ad358afd57c5fbd061f5ac58781ec37ddff19
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2023-08-21 22:38:48 -0300

    Customizando dapper com auxilio do entity e implementando CQRS

---
+++ Curso.Dapper.ComEntity.Api/Application/Handlers/Commands/Curso/CadastrarCursoCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Curso.Dapper.ComEntity.Api.Application.Interfaces;
+using Curso.Dapper.ComEntity.Api.Domain.Commands.Curso;
+using Curso.Dapper.ComEntity.Api.Domain.Services.Interfaces;
+
+namespace Curso.Dapper.ComEntity.Api.Application.Handlers.Commands.Curso
+{
+    public class CadastrarCursoCommandHandler : ICommandHandler<CadastrarCursoCommand>
+    {
+        private readonly ICursoDapperDomainService _cursoDapperDomainService;
+
+        public CadastrarCursoCommandHandler(ICursoDapperDomainService cursoDapperDomainService) 
+            => _cursoDapperDomainService = cursoDapperDomainService;
+
+        public async Task Handle(CadastrarCursoCommand command, CancellationToken cancellationToken)
+        {
+            //Validacao do command
+
+            //Mapper para converter o command em entidade
+            var curso = new Domain.Entities.Curso
+            {
+                Nome = command.Nome,
+                Descricao = command.Descricao,
+                Coordenador = command.Coordenador,
+                Professor = command.Professor,
+                TipoCurso = command.TipoCurso,
+                DataCriacao = DateTime.Now
+            };
+
+            //Validacao do curso
+
+            await _cursoDapperDomainService.CadastrarCurso(curso);
+        }
+    }
+}
---
+++ Curso.Dapper.ComEntity.Api/Domain/Common/Command.cs
@@ -0,0 +1,11 @@
+﻿namespace Curso.Dapper.ComEntity.Api.Domain.Common;
+
+public abstract class Command
+{
+    public DateTime DataExecucao { get; private set; }
+
+    protected Command()
+    {
+        DataExecucao = DateTime.Now;
+    }
+}
